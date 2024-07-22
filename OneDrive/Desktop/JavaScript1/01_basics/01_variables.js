37841d4ffc0d6133e4d76e918f603ff71af05bb2
Nikhil <nikhilltopre5@gmail.com>
2024-07-22 16:04:39 +0530

    first commit

---
+++ OneDrive/Desktop/JavaScript1/01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2 // not allowed
+console.log(accountId);
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+console.log([accountId, accountEmail, accountPassword, accountCity, accountState])
